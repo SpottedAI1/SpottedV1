ee125fc6686e9d4e07d64a7e9bca6f01ef3e2c59
PonVignesh <ponvignesh6@gmail.com>
2025-11-24 02:39:20 +0530

    signup page added

--- src/components/LogoSec.jsx
+++ src/components/LogoSec.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 const LogoSec = () => {
   return (
     <div className="w-full mb-2">
-      <div className='flex items-center gap-3 px-1 pl-3 py-3 '>
+      <div className="flex items-center gap-3 px-1 pl-3 py-3 ">
         <Image src="/Icon.png" alt="Spotted" width={28} height={28} />
-        <h1 className='font-bold text-2xl text-black'>Spotted</h1>
+        <h1 className="font-bold text-2xl text-black">Spotted</h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LogoSec
-
-
+export default LogoSec;
