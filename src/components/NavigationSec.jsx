7ff12fe2f036688a0554ee0b463040246fd6690b
PonVignesh <ponvignesh6@gmail.com>
2025-11-29 09:54:15 +0530

    updating navigationSec page to clear vercel hydration error

--- src/components/NavigationSec.jsx
+++ src/components/NavigationSec.jsx
@@ -1,10 +1,19 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const NavigationSec = () => {
+  //To prevent hydration error
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   const [isSettingsOpen, setIsSettingsOpen] = useState(false);
   const pathname = usePathname();
 
