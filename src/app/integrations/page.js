7419de1d27afd8fb3c4b0526b149b2f9e11d7d5f
PonVignesh <ponvicky6@gmail.com>
2025-11-29 13:04:41 +0530

    Fix search-results Suspense issue and build errors

--- src/app/integrations/page.js
+++ src/app/integrations/page.js
@@ -1,5 +1,4 @@
 import SideBar from "@/components/SideBar";
-import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 "use client";
-import LogoSec from "@/components/LogoSec";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 export default function SignupPage() {
